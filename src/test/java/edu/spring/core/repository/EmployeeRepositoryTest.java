0efc1562891e3cd7b09419b42bc1ed75a61bf99b
Efren Galarza <efrenpgc2602@gmail.com>
2023-01-15 22:03:19 -0500

    added Basic Persistence test

--- src/test/java/edu/spring/core/repository/EmployeeRepositoryTest.java
+++ src/test/java/edu/spring/core/repository/EmployeeRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 /*
 Sirve para probar componentes solo de la capa de persistencia
@@ -22,38 +23,61 @@ public class EmployeeRepositoryTest {
     private Employee employee;
 
     @BeforeEach
-    public void poblateData() {
+    public void setup() {
         employee = new Employee(null, "Efren", "Galarza", "efren@gmail.com");
     }
 
     @Test
     void testSaveEmployee() {
-//        Give
+//        Give - dado una condicion
         var employee = new Employee(null, "Efren", "Galarza", "efren@gmail.com");
-//        When
+//        When - accion o el comportamiento que vamos a probar
         var employeeSave = employeeRepository.save(employee);
-//        Then
+//        Then - verificar la salida
         assertThat(employeeSave).isNotNull();
         assertThat(employeeSave.getId()).isGreaterThan(0);
     }
 
     @Test
     void testAllEmployees() {
-//        Give
+//        Give - Se especifica el escenario, las precondiciones
         employeeRepository.save(employee);
-//        When
+//        When - Las condiciones de la lista que se van a ejecutar
         List<Employee> employees = employeeRepository.findAll();
-//        Then
+//        Then - El resultado esperado, las validaciones a realizar
         assertThat(employees.size()).isEqualTo(1);
     }
 
     @Test
     void testFindEmployeeById() {
+//        Give - Dado el empleado guardado en la db
         employeeRepository.save(employee);
-//        when
-        var employeeR = employeeRepository.findById(employee.getId()).get();
-//        then
-        assertThat(employeeR).isNotNull();
+//        When - Cuando se busque a ese empleado por id
+        var employeeR = employeeRepository.findById(employee.getId());
+//        Then - Confirmar que el empleado buscado no sea nulo
+        assertThat(employeeR.orElse(null)).isNotNull();
     }
-    
+
+    @Test
+    void testUpdateEmployeeById() {
+//        Give - Dado un employee en la db
+        var employeeSave = employeeRepository.save(employee);
+//        When - Cuando sea actualizado
+        employeeSave.setName("Gisell");
+        var employeeUpdate = employeeRepository.save(employeeSave);
+//        Then - Verificar que los cambios realizados sean correctos
+        assertThat(employeeUpdate.getName()).isEqualTo("Gisell");
+    }
+
+    @Test
+    void testDeleteEmployee() {
+//        Give - Dado un employee en la db
+        var employeeSave = employeeRepository.save(employee);
+//        When - Cuando el empleado sea borrado por id
+        long id = employeeSave.getId();
+        employeeRepository.deleteById(id);
+//        Then - Verificar que el empleado no exista en db
+        assertFalse(employeeRepository.existsById(id));
+    }
+
 }
